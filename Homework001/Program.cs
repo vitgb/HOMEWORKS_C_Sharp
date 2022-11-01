1f6c7331fc66a4bbf2fca5d6b9115425c955841a
unknown <v1599199@gmail.com>
2022-11-01 22:13:56 +0300

    Exercise_4 have done

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -3,7 +3,7 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-
+/*
 Console.WriteLine("Input a number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
@@ -15,5 +15,33 @@ if (a>max) max=a;
 if (b>max) max=b;
 
 Console.Write($"max = {max}");
+*/
 
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 
+Console.WriteLine("Input a first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input a third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (number1>max)
+{
+    max = number1;
+}
+if (number2>max)
+{
+    max = number2;
+}
+if (number3>max)
+{
+    max = number3;
+}
+Console.WriteLine(max);
