5e915acd375260cae07c77ddf4d98df9868fc802
unknown <v1599199@gmail.com>
2022-11-01 22:23:03 +0300

    Exercise_6 have done

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -21,7 +21,7 @@ Console.Write($"max = {max}");
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-
+/*
 Console.WriteLine("Input a first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
@@ -45,3 +45,17 @@ if (number3>max)
     max = number3;
 }
 Console.WriteLine(max);
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Input a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
