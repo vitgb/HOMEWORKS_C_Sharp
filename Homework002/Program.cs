ed881b036954308aa5831785ebda9e2c0550d7f5
unknown <v1599199@gmail.com>
2022-11-06 15:25:49 +0300

    first task decided

---
+++ Homework002/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+/*
+int TenthOfNumber (int num)
+{
+    num = (num / 10) % 10;
+    return num;
+}
+Console.WriteLine("Input a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number/100 > 1 && number/100 < 10)
+{
+    int result = TenthOfNumber(number);
+    Console.WriteLine($"The Tenth part of a number {number} is {result}");
+}
+else
+{
+    Console.WriteLine("WRONG NUMBER");
+}
+*/    
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+//int ThirdDigit(int number)
+
+
+
+
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
